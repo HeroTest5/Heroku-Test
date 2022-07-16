43ae21b40847b984801381d4f41f8b50296eb403
HeroTest5 <109363788+HeroTest5@users.noreply.github.com>
2022-07-16 23:19:24 +0530

    Create mount.sh

---
+++ mount.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/thereisnothinginhere/heroleechbot0sdaf/master/tobrot/helper_funcs/rclone.conf
+ls
+mkdir drive
+rclone mount Shared: drive --vfs-cache-mode writes --daemon --config=rclone.conf
+cd drive
+pwd
+ls frive
+ls -R drive
