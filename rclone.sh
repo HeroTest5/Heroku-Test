38a9912f456cd69aef26774b7fd8eba97c20717b
HeroTest5 <109363788+HeroTest5@users.noreply.github.com>
2022-07-16 19:50:35 +0530

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,3 +1,3 @@
 curl https://rclone.org/install.sh | sudo bash
 curl 'https://raw.githubusercontent.com/HeroBenHero/HeroBenHero.github.io/main/Kodi/Latest/rclone.conf' > rclone.conf
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" Shared2:"Ben 10" --config=rclone.conf --progress --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" Shared2:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
