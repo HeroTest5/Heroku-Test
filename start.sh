0e2fd3c7ca439d2655f9cdef1832134b4b11e4d8
HeroTest5 <109363788+HeroTest5@users.noreply.github.com>
2022-08-09 22:05:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 pwd
 ls
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness:"Ben 10" --config=rclone.conf --progress --drive-acknowledge-abuse
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" Shared2:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness1:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness:"Ben 10" --progress --config=rclone.conf --progress --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" Shared2:"Ben 10" --progress --config=rclone.conf --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness1:"Ben 10" --progress --config=rclone.conf --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness:"Ben 10" --progress --config=rclone.conf --drive-acknowledge-abuse
